e0e2052c25c482cace3be887c5e27e3f6dc92cac
Pedro Lucas <plfm3k@gmail.com>
2025-11-19 20:23:22 -0400

    fix: renoma a tabela bloco_textos para blocos_texto

--- database/migrations/2025_11_19_015001_create_bloco_textos_table.php
+++ database/migrations/2025_11_19_015001_create_bloco_textos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bloco_textos', function (Blueprint $table) {
+        Schema::create('blocos_texto', function (Blueprint $table) {
             $table->id();
             $table->foreignId('documento_id')->constrained()->onDelete('cascade');
             $table->unsignedInteger('ordem');
